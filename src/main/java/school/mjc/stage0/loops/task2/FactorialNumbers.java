223eb3d01d4cf727c4c6fb7f30aa00f24b98bb6d
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 16:22:53 +0500

    Interesting tasks.

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,10 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int factorial = 1;
+        int factorial = 0;
         int i = 2;
         int count = 1;
         while(count <= printToInclusive){
+            if(factorial == 0){
+                System.out.println(1);
+                factorial = 1;
+            }
             System.out.println(factorial);
             factorial *= i;
             i++;
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,9 +3,11 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int numbers = 0;
-        while(numbers <= multiplyByAndToInclusive){
-            System.out.println(numbers * numbers);
-            numbers ++;
+        int i = 0;
+        while(i <= multiplyByAndToInclusive){
+            System.out.println(numbers);
+            numbers += 5;
+            i++;
         }
     }
 }
