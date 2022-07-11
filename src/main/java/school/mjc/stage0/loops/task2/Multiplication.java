1a354cd83eb1df6deb801c51440902e133b14555
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 16:37:04 +0500

    Interesting tasks.

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,10 +4,19 @@ public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int numbers = 0;
         int i = 0;
-        while(i <= multiplyByAndToInclusive){
-            System.out.println(numbers);
-            numbers += 5;
-            i++;
+        if(multiplyByAndToInclusive < 0){
+            while(i >= multiplyByAndToInclusive){
+                System.out.println(numbers);
+                numbers -= 5;
+                i--;
+            }
+        }
+        else{
+            while(i <= multiplyByAndToInclusive){
+                System.out.println(numbers);
+                numbers += 5;
+                i++;
+            }
         }
     }
 }
